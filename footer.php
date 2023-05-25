299ca4323cc628495875d1a902abe675c095adfa
Aurelie Fort <aurelie.fort@live.fr>
2023-05-25 16:17:10 +0200

    fichiers de base

---
+++ footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+			<footer id="site-footer" class="header-footer-group">
+
+				<div class="section-inner">
+
+					<p>Mentions légales</p>
+
+						
+				</div><!-- .section-inner -->
+
+			</footer><!-- #site-footer -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
