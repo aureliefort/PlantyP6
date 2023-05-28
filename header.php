4ea1d8845a8435c5c52d5aee92d15d62b83ec714
Aurelie Fort <aurelie.fort@live.fr>
2023-05-28 10:39:25 +0200

    fichiers de base

--- header.php
+++ header.php
@@ -18,9 +18,15 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+		
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 
 		<?php wp_head(); ?>
+
+		
 
 	</head>
 
