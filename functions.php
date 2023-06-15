ef5ff89ea53de68874c0039a9e39b1fd4fd0905f
aureliefort <130485302+aureliefort@users.noreply.github.com>
2023-06-16 01:34:51 +0200

    maj responsive et hook nav
    ajout du css en version mobile et création du hook nav

--- functions.php
+++ functions.php
@@ -1,48 +1,55 @@
 <?php
+/**
+ * OceanWP Child Theme Functions
+ *
+ * When running a child theme (see http://codex.wordpress.org/Theme_Development
+ * and http://codex.wordpress.org/Child_Themes), you can override certain
+ * functions (those wrapped in a function_exists() call) by defining them first
+ * in your child theme's functions.php file. The child theme's functions.php
+ * file is included before the parent theme's file, so the child theme
+ * functions will be used.
+ *
+ * Text Domain: oceanwp
+ * @link http://codex.wordpress.org/Plugin_API
+ *
+ */
 
-// Action qui permet de charger des scripts dans notre thème
-add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-function theme_enqueue_styles(){
-    // Chargement du style.css du thème parent Twenty Twenty
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    // Chargement du css/theme.css pour nos personnalisations
-    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
-    // Chargement du /css/shortcodes/banniere-titre.css pour notre shortcode banniere titre
-    wp_enqueue_style('image-produit-shortcode', get_stylesheet_directory_uri() . '/css/shortcodes/image-produit.css', array(), filemtime(get_stylesheet_directory() . '/css/shortcodes/image-produit.css'));
+/**
+ * Load the parent style.css file
+ *
+ * @link http://codex.wordpress.org/Child_Themes
+ */
+function oceanwp_child_enqueue_parent_style() {
 
+	// Dynamically get version number of the parent stylesheet (lets browsers re-cache your stylesheet when you update the theme).
+	$theme   = wp_get_theme( 'OceanWP' );
+	$version = $theme->get( 'Version' );
+
+	 // Load the stylesheet.
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'oceanwp-style' ), $version );
+	
+	// Chargement du css/theme.css pour nos personnalisations
+	 wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+   
+   
 }
 
+add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
 
-/* SHORTCODES */
+	//ajout onglet admin si connecté
+	
+function add_admin_menu_item($items, $args) {
+	
+	if (is_user_logged_in() && $args->theme_location == 'main_menu') {
 
-
-// Je dis à wordpress que j'ajoute un shortcode 'banniere-titre'
-add_shortcode('image-produit', 'image_produit_func');
-// Je génère le html retourné par mon shortcode
-function image_produit_func($atts)
-{
-    //Je récupère les attributs mis sur le shortcode
-    $atts = shortcode_atts(array(
-        'src' => '',
-        'titre' => 'Titre'
-    ), $atts, 'image-produit');
-
-    //Je commence à récupéré le flux d'information
-    ob_start();
-
-    if ($atts['src'] != "") {
-        ?>
-
-        <div class="image-produit" style="background-image: url(<?= $atts['src'] ?>)">
-            <h3 class="titre"><?= $atts['titre'] ?></h3>
-        </div>
-
-        <?php
+		$items ='<li class="menu-item-26"><a href="http://localhost:8888/PlantyP6/nous-contacter/">Nous rencontrer</a></li>
+					<li class="menu-item-27"><a href="http://localhost:8888/PlantyP6/admin/">Admin</a></li>
+					<li class="menu-item-25"><a href="http://localhost:8888/PlantyP6/precommande/">Commander</a></li>';
+		
     }
-
-    //J'arrête de récupérer le flux d'information et le stock dans la fonction $output
-    $output = ob_get_contents();
-    ob_end_clean();
-
-    return $output;
+		return $items;
+	
 }
+
+add_filter('wp_nav_menu_items','add_admin_menu_item', 10, 2);
+
